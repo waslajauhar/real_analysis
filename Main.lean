8598a95319dad79a56dff4faaa572504988e53b9
Wasla Jauhar <waslajauhar@gmail.com>
2025-05-27 22:55:05 +0530

    Initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import UnderstandingAnalysis
+
+def main : IO Unit :=
+  IO.println s!"Hello!"
---
+++ UnderstandingAnalysis.lean
@@ -0,0 +1,7 @@
+-- This module serves as the root of the `UnderstandingAnalysis` library.
+-- Import modules here that should be built as part of the library.
+--import UnderstandingAnalysis.Basic
+
+-- ch1
+import UnderstandingAnalysis.ch1.theroems.thm111
+
