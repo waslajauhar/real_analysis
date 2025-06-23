cafeb7bd059c731fd9cd7a3495c608a2a7fa72a6
Wasla Jauhar <waslajauhar@gmail.com>
2025-06-23 12:18:27 +0530

    added thm111

--- UnderstandingAnalysis/ch1/theroems/thm111.lean
+++ UnderstandingAnalysis/ch1/theroems/thm111.lean
@@ -1,6 +1,8 @@
 -- Theorem 1.1.1
 -- There is no rational number whose square is 2.
 
+-- Still in progress --
+
 def even (n : Nat) : Prop := ∃ k : Nat, n = 2 * k
 
 theorem not_even_and_coprime (a b : Nat) (h₁ : even a) (h₂ : even b) : False := 
@@ -8,7 +10,6 @@ theorem not_even_and_coprime (a b : Nat) (h₁ : even a) (h₂ : even b) : False
   | ⟨k1, hk1⟩ , ⟨k2, hk2⟩ =>
     False.elim (by
       admit)
-
 
 
 theorem sqrt2_irratioanl : ¬ ∃ a b : Nat, b != 0 ∧ Nat.gcd a b = 1 ∧ a^2 = 2 * b^2 := by
@@ -32,4 +33,3 @@ theorem sqrt2_irratioanl : ¬ ∃ a b : Nat, b != 0 ∧ Nat.gcd a b = 1 ∧ a^2 
         admit
 
     exact not_even_and_coprime a b even_a even_b
-
